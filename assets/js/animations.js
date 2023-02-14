842e875df2b315e67cd7197c5a34adf05359750d
Ayslan Gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2023-02-14 18:01:45 -0300

    a

---
+++ assets/js/animations.js
@@ -0,0 +1,37 @@
+const debounce = (callback, wait) => {
+    let timeoutId = null;
+    return (...args) => {
+        window.clearTimeout(timeoutId);
+        timeoutId = window.setTimeout(() => {
+        callback.apply(null, args);
+        }, wait);
+    };
+}
+
+// elementos html com data animation
+const targetAnimationElements = document.querySelectorAll('[data-animation]');
+
+// classe do css
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 1) / 10);
+
+    targetAnimationElements.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);    
+        }
+    })
+
+}
+
+animeScroll();
+
+if(targetAnimationElements.length > 0){
+    // verifica se mexeu o mouse 
+    window.addEventListener('scroll',debounce(function(){
+        animeScroll();
+    },100))
+}
