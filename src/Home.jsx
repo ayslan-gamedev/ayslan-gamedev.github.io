365eb1a9b1b73f5bd9001a66d286c275c32163e3
Ayslan_Gamedev <ayslan_gamedev@disroot.org>
2025-04-24 13:25:00 -0300

    tring to resolve conflicts

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="Home">
+      
+      <div className="hello">
+        <h2>Hello World!</h2>
+        <h1>I'm Ayslan Fontes</h1>
+        <h2>Software Engineer and game developer</h2>
+      </div>
+
+      <div className="footer">
+        <h2>Searching for work experience? <br/> scroll down </h2>
+      </div>
+    </div>
+  )
+}
